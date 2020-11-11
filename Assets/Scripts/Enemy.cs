b5a92072384bd99b3c46320aa38732cbbf92bd0d
Nick Bykon <nikita.bikon@gmail.com>
2020-11-11 14:52:01 +0200

    GameManager, enemy health, lives

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,12 +6,30 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     public float speed = 10f;
+    public int health = 100;
+    public int value = 50;
 
+    public GameObject deathEffect;
     private Transform target;
     private int wavepointIndex = 0;
     void Start() {
         target = Waypoints.points[0];
     }
+
+    public void TakeDamage(int amount){
+        health -= amount;
+        if(health <= 0){
+            Die();
+        }
+    }
+
+    void Die(){
+        PlayerStats.Money += value;
+        GameObject effect = Instantiate(deathEffect, transform.position, Quaternion.identity) as GameObject;
+        Destroy(effect, 5f);
+        Destroy(gameObject);
+    }
+
     void Update() {
         Vector3 dir = target.position - transform.position;
         transform.Translate(dir.normalized*speed*Time.deltaTime, Space.World);
@@ -26,10 +44,15 @@ public class Enemy : MonoBehaviour
     {
         if (wavepointIndex >= Waypoints.points.Length - 1)
         {
-            Destroy(gameObject);
+            EndPath();
             return;
         }
         wavepointIndex++;
         target = Waypoints.points[wavepointIndex];
+    }
+
+    void EndPath(){
+        PlayerStats.Lives--;
+        Destroy(gameObject);
     }
 }
