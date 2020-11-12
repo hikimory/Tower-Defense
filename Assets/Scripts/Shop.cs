7521e7afac201a41cddf6d7b108e140352c1c262
Nick Bykon <nikita.bikon@gmail.com>
2020-11-12 12:35:17 +0200

    Laser Beamer

--- Assets/Scripts/Shop.cs
+++ Assets/Scripts/Shop.cs
@@ -6,6 +6,7 @@ public class Shop : MonoBehaviour
 {
     public TurretBlueprint standardTurret;
     public TurretBlueprint missileLauncher;
+    public TurretBlueprint laserBeamer;
 
     BuildManager buildManager;
 
@@ -20,5 +21,10 @@ public class Shop : MonoBehaviour
     public void SelectMissileLauncher(){
         Debug.Log("Missile Launcher Selected");
         buildManager.SelectTurretToBuild(missileLauncher);
+    }
+
+    public void SelectLaserBeamer(){
+        Debug.Log("Laser Beamer Selected");
+        buildManager.SelectTurretToBuild(laserBeamer);
     }
 }
