613fa0047a2c1480801e275093f0bc6f939fe106
Nick Bykon <nikita.bikon@gmail.com>
2020-11-13 16:05:00 +0200

    Slowing

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,54 +5,34 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    public float speed = 10f;
-    public int health = 100;
-    public int value = 50;
-
+    public float startSpeed = 10f;
+    
+    [HideInInspector]
+    public float speed;
+    public float health = 100;
+    public int worth = 50;
     public GameObject deathEffect;
-    private Transform target;
-    private int wavepointIndex = 0;
-    void Start() {
-        target = Waypoints.points[0];
-    }
 
-    public void TakeDamage(int amount){
+    void Start(){
+        speed = startSpeed;
+    }
+    public void TakeDamage(float amount){
         health -= amount;
         if(health <= 0){
             Die();
         }
     }
 
+    public void Slow(float pct){
+        speed = startSpeed * (1f - pct);
+    }
+
     void Die(){
-        PlayerStats.Money += value;
+        PlayerStats.Money += worth;
         GameObject effect = Instantiate(deathEffect, transform.position, Quaternion.identity) as GameObject;
         Destroy(effect, 5f);
         Destroy(gameObject);
     }
 
-    void Update() {
-        Vector3 dir = target.position - transform.position;
-        transform.Translate(dir.normalized*speed*Time.deltaTime, Space.World);
 
-        if (Vector3.Distance(transform.position, target.position) < 0.4f)
-        {
-            GetNextWaypoint();
-        }
-    }
-
-    void GetNextWaypoint()
-    {
-        if (wavepointIndex >= Waypoints.points.Length - 1)
-        {
-            EndPath();
-            return;
-        }
-        wavepointIndex++;
-        target = Waypoints.points[wavepointIndex];
-    }
-
-    void EndPath(){
-        PlayerStats.Lives--;
-        Destroy(gameObject);
-    }
 }
--- Assets/Scripts/Turret.cs
+++ Assets/Scripts/Turret.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Turret : MonoBehaviour
 {
     private Transform target;
+    private Enemy targetEnemy;
 
     [Header("General")]
     public float range = 15f;
@@ -16,6 +17,10 @@ public class Turret : MonoBehaviour
 
     [Header("Use laser")]
     public bool useLaser = false; 
+
+    public int damageOverTime = 30;
+    public float slowAmount = .5f;
+
     public LineRenderer lineRenderer;
     public ParticleSystem impactEffect;
     public Light impactLight;
@@ -46,6 +51,7 @@ public class Turret : MonoBehaviour
         }
         if(nearestEnemy != null && shortestDistance <= range){
             target = nearestEnemy.transform;
+            targetEnemy = nearestEnemy.GetComponent<Enemy>();
         }else{
             target = null;
         }
@@ -89,6 +95,9 @@ public class Turret : MonoBehaviour
     }
 
     void Laser(){
+        targetEnemy.TakeDamage(damageOverTime * Time.deltaTime);
+        targetEnemy.Slow(slowAmount);
+
         if(!lineRenderer.enabled){
             lineRenderer.enabled = true;
             impactEffect.Play();
